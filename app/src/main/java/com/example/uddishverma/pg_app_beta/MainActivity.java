5cfdb2dcd2f7d9cbf845906eafe80f123e63c34f
Uddish Verma <uddishverma22@gmail.com>
2016-08-07 21:53:41 +0530

    Added Register activity with material edit text library

---
+++ app/src/main/java/com/example/uddishverma/pg_app_beta/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.uddishverma.pg_app_beta;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button registerPGBtn;
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        registerPGBtn = (Button) findViewById(R.id.register_pg_btn);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    //This function opens the register pg activity
+    public void openRegisterPgActivity(View view)   {
+        Intent i = new Intent(this, RegisterPG.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/example/uddishverma/pg_app_beta/RegisterPG.java
@@ -0,0 +1,13 @@
+package com.example.uddishverma.pg_app_beta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterPG extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_pg);
+    }
+}
