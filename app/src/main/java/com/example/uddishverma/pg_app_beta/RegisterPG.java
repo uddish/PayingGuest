9097756124bb4ce92b8383968ad2e20bc339a907
Uddish Verma <uddishverma22@gmail.com>
2016-08-08 02:10:49 +0530

    Register activity almost completed with complete content

--- app/src/main/java/com/example/uddishverma/pg_app_beta/RegisterPG.java
+++ app/src/main/java/com/example/uddishverma/pg_app_beta/RegisterPG.java
@@ -2,6 +2,7 @@ package com.example.uddishverma.pg_app_beta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class RegisterPG extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class RegisterPG extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_pg);
+
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 }
