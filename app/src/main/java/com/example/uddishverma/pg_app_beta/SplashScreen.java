cf7efb754bf13a7ae5f08cc2602f68ccb348b438
shankymahajan <shashankmahajan18@gmail.com>
2016-09-29 02:56:47 +0530

    Splash screen Update now include Introduction screens(material design)

--- app/src/main/java/com/example/uddishverma/pg_app_beta/SplashScreen.java
+++ app/src/main/java/com/example/uddishverma/pg_app_beta/SplashScreen.java
@@ -3,11 +3,14 @@ package com.example.uddishverma.pg_app_beta;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.TaskStackBuilder;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import static com.example.uddishverma.pg_app_beta.R.id.imageView;
 
 /**
  * Opens up in the starting of the app
@@ -17,36 +20,40 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashScreen extends Activity {
 
-    ImageView imageView;
+    //ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        TaskStackBuilder.create(this)
+                .addNextIntentWithParentStack(new Intent(this, MainActivity.class))
+                .addNextIntent(new Intent(this, IntroActivity.class))
+                .startActivities();
 
-        imageView = (ImageView) findViewById(R.id.splash_image);
-
-        Animation animationUtils= AnimationUtils.loadAnimation(getBaseContext(),R.anim.anim_splash_screen);
-        imageView.startAnimation(animationUtils);
-        animationUtils.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                finish();
-
-                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-                    startActivity(intent);
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
+//        imageView = (ImageView) findViewById(R.id.splash_image);
+//
+//        Animation animationUtils= AnimationUtils.loadAnimation(getBaseContext(),R.anim.anim_splash_screen);
+//        imageView.startAnimation(animationUtils);
+//        animationUtils.setAnimationListener(new Animation.AnimationListener() {
+//            @Override
+//            public void onAnimationStart(Animation animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationEnd(Animation animation) {
+//                finish();
+//
+//                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+//                    startActivity(intent);
+//
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animation animation) {
+//
+//            }
+//        });
     }
 }
