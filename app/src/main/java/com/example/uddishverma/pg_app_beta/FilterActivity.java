2a8771c8b6b3754f6feb61afce064bf0c5a6e66a
ng0227 <namangupta0227@gmail.com>
2016-10-20 16:43:54 +0530

    creating locality List and College List using stringResources

--- app/src/main/java/com/example/uddishverma/pg_app_beta/FilterActivity.java
+++ app/src/main/java/com/example/uddishverma/pg_app_beta/FilterActivity.java
@@ -58,6 +58,9 @@ public class FilterActivity extends AppCompatActivity {
 
     Toolbar toolbar;
 
+    String[] localityListFromResources;
+    String[] collegeListFromResources;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -79,8 +82,17 @@ public class FilterActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.filter_toolbar);
         setSupportActionBar(toolbar);
 
+        localityListFromResources=getResources().getStringArray(R.array.locality);
 
-        locality = new Filter_POJO[]
+        locality=new Filter_POJO[localityListFromResources.length];
+        for(int i=0;i<localityListFromResources.length;i++)
+        {
+            locality[i]=new Filter_POJO(localityListFromResources[i]);
+        }
+
+
+
+        /*locality = new Filter_POJO[]
                 {
                         new Filter_POJO("Anand Vihar"),
                         new Filter_POJO("Dilshad Garden"),
@@ -94,14 +106,25 @@ public class FilterActivity extends AppCompatActivity {
                         new Filter_POJO("Vikas Puri"),
                         new Filter_POJO("Pascim Vihar")
                 };
+*/
 
         localityList = new ArrayList<Filter_POJO>();
         localityList.addAll(Arrays.asList(locality));
 
 
+
 /*************************************************************************/
         //Adding colleges
-        colleges=new Filter_POJO[]
+
+        collegeListFromResources=getResources().getStringArray(R.array.institutes);
+
+        colleges=new Filter_POJO[collegeListFromResources.length];
+        for(int i=0;i<collegeListFromResources.length;i++)
+        {
+            colleges[i]=new Filter_POJO(collegeListFromResources[i]);
+        }
+
+       /* colleges=new Filter_POJO[]
                 {
 
                         new Filter_POJO("DTU"),
@@ -113,7 +136,7 @@ public class FilterActivity extends AppCompatActivity {
                         new Filter_POJO("USIT"),
                         new Filter_POJO("IIIT DELHI"),
                         new Filter_POJO("JAYPEE NOIDA")
-                };
+                };*/
 
 
         collegeList = new ArrayList<Filter_POJO>();
